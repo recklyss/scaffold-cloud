b3179b311d5a25fecb137160897612b56fe6d5dd
Fatezhang <762344938@qq.com>
2019-07-28 19:14:12 +0800

    创建AO/BO sys_operate对应的，用于微服务之间的数据传递

---
+++ scaffold-business-api/scaffold-business-sys-api/src/main/java/com/cms/scaffold/micro/sys/ao/SysOperateAO.java
@@ -0,0 +1,42 @@
+package com.cms.scaffold.micro.sys.ao;
+
+import com.cms.scaffold.common.base.BaseEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+/**
+* 
+* @author: Mybatis Generator
+* @date: 2019-07-28 18:54:02
+*/
+@Getter
+@Setter
+public class SysOperateAO extends BaseEntity {
+    /** 用户名**/
+    private String userName;
+
+    private String realName;
+
+    /** 密码**/
+    private String pwd;
+
+    /** 手机号**/
+    private String mobilePhone;
+
+    /** 商户id**/
+    private Long partnerId;
+
+    /** 最近登录ip**/
+    private String loginIp;
+
+    /** 最近登录时间**/
+    private Date loginTime;
+
+    /** 状态**/
+    private Long status;
+
+    /** 钉钉系统用户id**/
+    private String openid;
+}
