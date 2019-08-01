3d995583381c005f7f5b5e7298cf1ed330c0f8aa
Fatezhang <762344938@qq.com>
2019-08-01 23:00:28 +0800

    导入bao

--- scaffold-business/scaffold-business-sys-service/src/main/java/com/cms/scaffold/micro/sys/domain/SysRoleMenu.java
+++ scaffold-business/scaffold-business-sys-service/src/main/java/com/cms/scaffold/micro/sys/domain/SysRoleMenu.java
@@ -1,12 +1,13 @@
 package com.cms.scaffold.micro.sys.domain;
 
 import com.cms.scaffold.common.annotation.TableName;
+import com.cms.scaffold.common.base.BaseEntity;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
 /**
-* 
+*
 * @author: Mybatis Generator
 * @date: 2019-08-01 19:57:21
 */
