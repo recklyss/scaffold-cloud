6eae6a6f12bf053d9c31900488c0f7a3184099cd
Fatezhang <762344938@qq.com>
2019-08-14 20:42:54 +0800

    增加RocketMq配置

---
+++ scaffold-core/scaffold-core-code/src/main/java/com/cms/scaffold/code/config/commonly/AliOnsMqConfig.java
@@ -0,0 +1,32 @@
+package com.cms.scaffold.code.config.commonly;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+/**
+ * @Author zhangjiaheng
+ * @Description
+ **/
+@Configuration
+@PropertySource(ignoreResourceNotFound = true, value = "classpath:code.properties")
+@Getter
+@Setter
+public class AliOnsMqConfig {
+
+    @Value("${rocket.mq.topic}")
+    String rocketMqTopic;
+    @Value("${rocket.mq.producer.id}")
+    String rocketMqProducerId;
+    @Value("${rocket.mq.access.key}")
+    String rocketMqAccessKey;
+    @Value("${rocket.mq.secret.key}")
+    String rocketMqSecretKey;
+    @Value("${rocket.mq.ons.addr}")
+    String rocketMqOnsAddr;
+    @Value("${rocket.mq.name.srv.addr}")
+    String rocketMqNameSrvAddr;
+
+}
