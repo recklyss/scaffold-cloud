5b2c1c79c18b18ebd05ea6df3ccb8cdf4ddb0107
Fatezhang <762344938@qq.com>
2019-08-19 16:37:10 +0800

    修改启动类注解

--- scaffold-route/scaffold-route-app/src/main/java/com/cms/scaffold/route/app/AppOperateApplication.java
+++ scaffold-route/scaffold-route-app/src/main/java/com/cms/scaffold/route/app/AppOperateApplication.java
@@ -2,7 +2,7 @@ package com.cms.scaffold.route.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableAsync;
@@ -13,7 +13,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
 @SpringBootApplication(scanBasePackages = {"com.cms.scaffold.route.app"})
 @EnableAspectJAutoProxy
 @EnableAsync
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients
 public class AppOperateApplication {
     public static void main(String[] args) {
--- scaffold-route/scaffold-route-operate/src/main/java/com/cms/scaffold/route/operate/RouteOperateApplication.java
+++ scaffold-route/scaffold-route-operate/src/main/java/com/cms/scaffold/route/operate/RouteOperateApplication.java
@@ -3,7 +3,7 @@ package com.cms.scaffold.route.operate;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
@@ -17,7 +17,7 @@ import org.springframework.scheduling.annotation.EnableAsync;
         "com.cms.scaffold.code.config.commonly"
 }, exclude = {DataSourceAutoConfiguration.class})
 @EnableAsync
-@EnableEurekaClient
+@EnableDiscoveryClient
 @EnableFeignClients(basePackages = "com.cms.scaffold.feign.*")
 @EnableHystrix
 @EnableAspectJAutoProxy
