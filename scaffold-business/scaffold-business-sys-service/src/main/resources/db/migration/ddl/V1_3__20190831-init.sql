bb53ff3f0b2ca050b9749ddeae92a8203eb8d0c0
Fatezhang <762344938@qq.com>
2019-08-31 23:41:33 +0800

    MQ消息表创建SQL脚本

---
+++ scaffold-business/scaffold-business-sys-service/src/main/resources/db/migration/ddl/V1_3__20190831-init.sql
@@ -0,0 +1,16 @@
+-- 创建MQ消息表
+CREATE TABLE `sys_message` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `message_id` varchar(255) NOT NULL COMMENT '消息ID',
+  `message_key` varchar(255) NOT NULL COMMENT '消息KEY',
+  `operate` varchar(255) NOT NULL DEFAULT '' COMMENT '操作',
+  `request_no` varchar(255) NOT NULL DEFAULT '' COMMENT '请求流水号',
+  `status` int(11) unsigned NOT NULL DEFAULT '1' COMMENT '1-消费正常 2-消费异常',
+  `request_param` varchar(255) NOT NULL DEFAULT '' COMMENT '请求参数',
+  `response_param` varchar(255) NOT NULL DEFAULT '' COMMENT '响应参数',
+  `add_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '添加时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '修改时间',
+  `trigger_num` int(5) unsigned NOT NULL DEFAULT '0' COMMENT '请求次数',
+  `tags` varchar(255) NOT NULL COMMENT '消息TAG',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT='角色表';
