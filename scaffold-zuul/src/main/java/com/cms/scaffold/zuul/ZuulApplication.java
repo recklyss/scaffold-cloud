395ce421f25914c4715d752c91526a2a314a6f0b
Fatezhang <762344938@qq.com>
2019-11-27 09:37:49 +0800

    增加 zuul 网关模块

---
+++ scaffold-zuul/src/main/java/com/cms/scaffold/zuul/ZuulApplication.java
@@ -0,0 +1,19 @@
+package com.cms.scaffold.zuul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+/**
+ * Zuul 网关
+ *
+ * @author jiaheng.zhang
+ */
+@SpringBootApplication
+@EnableZuulProxy
+public class ZuulApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZuulApplication.class, args);
+    }
+}
