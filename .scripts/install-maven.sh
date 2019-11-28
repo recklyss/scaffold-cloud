e711446a821856606301de3b703ff6682c966d29
Fatezhang <762344938@qq.com>
2019-11-28 23:50:31 +0800

    修复 maven 安装脚本

--- .scripts/install-maven.sh
+++ .scripts/install-maven.sh
@@ -3,13 +3,13 @@ if [ `command -v mvn` ];then
     echo '=======================maven 已经安装======================='
 else
     echo 'maven 没有安装 准备安装maven'
-    echo -e  "Begin to install maven,Please waiting..."
+    echo -e "Begin to install maven,Please waiting..."
     wget https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
     tar zxvf apache-maven-3.6.3-bin.tar.gz
-    mv apache-maven-3.6.3 /usr/local/maven
-    echo "#MAVEN_HOME" >>/etc/profile
-    echo "export MAVEN_HOME=/usr/local/maven" >>/etc/profile
-    echo "export PATH=\$PATH:\$MAVEN_HOME/bin" >>/etc/profile
-    source /etc/profile
+    sudo mv apache-maven-3.6.3 /usr/local/maven
+    sudo echo "#MAVEN_HOME" >>/etc/profile
+    sudo echo "export MAVEN_HOME=/usr/local/maven" >>/etc/profile
+    sudo echo "export PATH=\$PATH:\$MAVEN_HOME/bin" >>/etc/profile
+    sudo source /etc/profile
     echo -e "Maven installation completed"
 fi
