877d6126cc47e4cac3d927ec15f59e29a2ccf57b
Fatezhang <762344938@qq.com>
2019-11-29 09:14:47 +0800

    还原删除的脚本

---
+++ .scripts/recreate-docker-image.sh
@@ -0,0 +1,16 @@
+#!/bin/bash -eu
+
+echo "============start to package with maven and recreate docker image=============="
+cd ..
+
+SERVICE_FOLDERS=( scaffold-eureka scaffold-zuul )
+path=
+for (( i = 0; i < ${#SERVICE_FOLDERS[@]}; i++ )); do
+    path=${SERVICE_FOLDERS[${i}]}
+    echo "进入目录 >>>> cd ${path}"
+    cd "${path}"
+    pwd
+    mvn clean package docker:build -Ptest
+    cd -
+done
+echo "============                      create end                     =============="
