6d8c9114cfafd598e5650f70b2ac100c423e2832
Fatezhang <762344938@qq.com>
2019-11-29 23:34:01 +0800

    修复安装maven 步骤 赋予文件权限

--- .scripts/install-maven.sh
+++ .scripts/install-maven.sh
@@ -3,16 +3,10 @@ if [ `command -v mvn` ];then
     echo '=======================maven 已经安装======================='
 else
     echo -e "Begin to install maven,Please waiting..."
-    yum -y install expect
-    set timeout 2
-    spawn su
-    expect "Password:"
-    exec sleep 1
-    send "emhhbmc=1ZJH\r"
-    expect "#"
-    interact
     wget https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
     tar zxvf apache-maven-3.6.3-bin.tar.gz
+    chmod -R 777 /usr/local
+    chmod 777 /etc/profile
     cp -r apache-maven-3.6.3 /usr/local/maven
     echo "export MAVEN_HOME=/usr/local/maven" >>/etc/profile
     echo "export PATH=\$PATH:\$MAVEN_HOME/bin" >>/etc/profile
