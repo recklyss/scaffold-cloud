e1600232a15990ef8d5fd2013be77ec7d195409e
Fatezhang <762344938@qq.com>
2019-11-29 10:42:09 +0800

    还原安装maven 步骤

---
+++ .scripts/install-maven.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eu
+if [ `command -v mvn` ];then
+    echo '=======================maven 已经安装======================='
+else
+    echo 'maven 没有安装 准备安装maven'
+    echo -e "Begin to install maven,Please waiting..."
+    wget https://mirrors.tuna.tsinghua.edu.cn/apache/maven/maven-3/3.6.3/binaries/apache-maven-3.6.3-bin.tar.gz
+    tar zxvf apache-maven-3.6.3-bin.tar.gz
+    sudo mv apache-maven-3.6.3 /usr/local/maven
+    sudo echo "#MAVEN_HOME" >>/etc/profile
+    sudo echo "export MAVEN_HOME=/usr/local/maven" >>/etc/profile
+    sudo echo "export PATH=\$PATH:\$MAVEN_HOME/bin" >>/etc/profile
+    sudo source /etc/profile
+    echo -e "Maven installation completed"
+fi
