160cf11de8b6d1db47c305156acd22f3c60a9715
Fatezhang <762344938@qq.com>
2019-12-01 15:26:17 +0800

    dockerfile 一律使用 maven 插件生成，删除 dockerfile

--- .scripts/recreate-docker-image.sh
+++ .scripts/recreate-docker-image.sh
@@ -1,15 +1,25 @@
-#!/bin/bash -eu
+#!/bin/bash
 
 echo "============start to package with maven and recreate docker image=============="
 
-SERVICE_FOLDERS=( scaffold-eureka scaffold-zuul )
+SERVICE_FOLDERS=(
+  scaffold-eureka
+  scaffold-zuul
+  scaffold-tx-manager
+  scaffold-business/scaffold-business-sys-service
+  scaffold-business/scaffold-business-job-service
+  scaffold-business/scaffold-business-thirdparty-service
+  scaffold-route/scaffold-route-app
+  scaffold-route/scaffold-route-openapi
+  scaffold-route/scaffold-route-operate
+)
 path=
 for (( i = 0; i < ${#SERVICE_FOLDERS[@]}; i++ )); do
     path=${SERVICE_FOLDERS[${i}]}
     echo "进入目录 >>>> cd ${path}"
     cd "${path}"
     pwd
-    mvn clean package docker:build -Ptest
+    mvn clean package docker:build -Pprod
     cd -
 done
 echo "============                      create end                     =============="
